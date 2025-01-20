10acf83b79208cb8b858f4dd9829dee56e782fc4
adelian14 <62575887+adelian14@users.noreply.github.com>
2025-01-20 06:50:43 +0200

    Update 2661. First Completely Painted Row or Column.cpp

--- LeetCode/2661. First Completely Painted Row or Column.cpp
+++ LeetCode/2661. First Completely Painted Row or Column.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        vector<int> a(n,m), b(m,n);
+        vector<int> a(n), b(m);
         vector<vector<int>> idx(n*m, vector<int>(2));
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
@@ -13,8 +13,8 @@ public:
         for(int i = 0; i < n*m; i++){
             int x = idx[arr[i]-1][0];
             int y = idx[arr[i]-1][1];
-            a[x]--, b[y]--;
-            if(!a[x] || !b[y]) return i;
+            a[x]++, b[y]++;
+            if(a[x]==m || b[y]==n) return i;
         }
         return NULL;
     }
